f8ef1712fbb6705e65a3a723c7ef24d12c19671d
Rhss <3567878896@qq.com>
2024-10-06 03:53:00 +0800

    BeanFactory

---
+++ src/main/java/com/example/springbeanfactorydemo/Bean/MyBean.java
@@ -0,0 +1,16 @@
+package com.example.springbeanfactorydemo.Bean;
+
+public class MyBean {
+    private String Message;
+
+    public String getMessage()
+    {
+        return Message;
+    }
+
+
+    public void setMessage(String message)
+    {
+        Message = message;
+    }
+}
---
+++ src/main/java/com/example/springbeanfactorydemo/SpringBeanFactoryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbeanfactorydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanFactoryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanFactoryDemoApplication.class, args);
+    }
+
+}
